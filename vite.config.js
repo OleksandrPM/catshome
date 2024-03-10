ae9c4ea0f795560ff90cb681c310e734b9486364
Oleksandr Moskaliuk <olekspm@gmail.com>
2024-03-10 16:35:20 +0100

    Added html-partials & scss structure

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,15 @@
-export default { base: "/catshome/" };
+// vite.config.js
+import { resolve } from "path";
+import handlebars from "vite-plugin-handlebars";
+
+export default {
+  base: "/catshome/",
+  plugins: [
+    handlebars({
+      partialDirectory: [
+        resolve(__dirname, "./src/partials"),
+        resolve(__dirname, "./src/partials/components"),
+      ],
+    }),
+  ],
+};
