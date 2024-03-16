a9d56a96ccb33ec702a40e470cc9ef017b03aa90
Oleksandr Moskaliuk <olekspm@gmail.com>
2024-03-16 11:52:07 +0100

    changed base prop in vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { resolve } from "path";
 import handlebars from "vite-plugin-handlebars";
 
 export default {
-  base: "/catshome",
+  base: "/",
   plugins: [
     handlebars({
       partialDirectory: [
